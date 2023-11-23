6a50fbb97b327f0aac8108be37ce324125fecc77
Martell <vovasamb@yandex.ru>
2023-11-23 19:33:32 +0300

    New sender token

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ MQTT_CLIENT_ID = 'TOIV_5'
 MQTT_TOPIC = "v1/devices/me/telemetry"
 MQTT_TOKEN = "6h1yrrbs9xsd2gc1xg84"
 MQTT_PARAMETERS = ["fan_state", "motion"]
-MQTT_SENDER_TOKEN = "jCKqf62Qc0P45U0Pdxo0"
+MQTT_SENDER_TOKEN = "nYmnRIrlOBItKYo1mUGB"
